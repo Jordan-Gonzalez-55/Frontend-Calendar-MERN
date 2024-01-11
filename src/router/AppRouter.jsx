26ff74bc19c121aed0d77cb6085ec2ecd3b2b233
Jordan <jordangon55@gmail.com>
2024-01-10 19:11:42 -0600

    Frontend V1.0 Login/Register Done

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,33 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { CalendarPage } from "../calendar";
+import { LoginPage } from "../auth";
+import { useAuthStore } from "../hooks";
+import { useEffect } from "react";
+
+export const AppRouter = () => {
+  const { status, checkAuthToken } = useAuthStore();
+  //const authStatus = "not-authenticated"; // 'authenticated'; // 'not-authenticated';
+  useEffect(() => {
+    checkAuthToken();
+  }, []);
+
+  if (status === "checking") {
+    return <h3>Cargando...</h3>;
+  }
+  return (
+    <Routes>
+      {status === "not-authenticated" ? (
+        <>
+          <Route path="/auth/*" element={<LoginPage />} />
+          <Route path="/*" element={<Navigate to="/auth/login" />} />
+        </>
+      ) : (
+        <>
+          <Route path="/" element={<CalendarPage />} />
+          {/* Sirve para limpiar la ruta */}
+          <Route path="/*" element={<Navigate to="/" />} />
+        </>
+      )}
+    </Routes>
+  );
+};
